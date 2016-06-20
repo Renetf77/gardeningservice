e9489cbd46ad8de82bcd62f2d481302936980942
rene.francisco <rene.francisco@IBR131.ibratecgrafica.com.br>
2016-06-20 14:26:24 -0300

    Botão Menu responsivo

--- index.php
+++ index.php
@@ -15,10 +15,13 @@
                 <div class="menu" >
                     <div class="gspace">
                         <a href="http://localhost/GardeningService/index.php" title="Site" ><img class="logo" alt="logo" title="logo" src="img/tratado/logomedio.png" ></a>
-                        <ul>
+                        <ul class="topnav">
                             <li><a href="#home" title="Gardening Services">Home</a></li>
                             <li><a href="#servicos" title="Serviços de Jardinagem">Serviços</a></li>
                             <li><a href="#contato" title="Entre em contato">Contato</a></li>
+                            <li class="icon">
+                                <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -170,7 +173,6 @@
 //                                else:
 //                                    echo "Dados incorretos";
 //                                endif;
-
                                     ?>
 
                                     <form action="index.php#contato" method="post">
@@ -203,16 +205,7 @@
         <div id="footer">
             <div class="gspace">
                 <footer>
-<!--                    <nav> 
-                        <h1>Gardening Service</h1>
-                        <ul>
-                            <li>
-                                <a target="_blank" title="Upinside treinamentos" href="//www.upinside.com.br">Upinside</a> 
-                            </li>
-                        </ul>
-                    </nav>-->
-<span class="footer">@copyright 2015 - <?= date('Y'); ?> - Gardening Services - Desenvolvido por</span>
-
+                    <span class="footer">@copyright 2015 - <?= date('Y'); ?> - Gardening Services - Desenvolvido por</span>
                 </footer>
             </div>
         </div>                
@@ -220,58 +213,64 @@
         <script type="text/javascript" src="js/jssor.slider.min.js"></script>
         <!-- use jssor.slider.debug.js instead for debug -->
         <script>
-            jssor_1_slider_init = function () {
+                                    jssor_1_slider_init = function () {
 
-                var jssor_1_SlideoTransitions = [
-                    [{b: 5500, d: 3000, o: -1, r: 240, e: {r: 2}}],
-                    [{b: -1, d: 1, o: -1, c: {x: 51.0, t: -51.0}}, {b: 0, d: 1000, o: 1, c: {x: -51.0, t: 51.0}, e: {o: 7, c: {x: 7, t: 7}}}],
-                    [{b: -1, d: 1, o: -1, sX: 9, sY: 9}, {b: 1000, d: 1000, o: 1, sX: -9, sY: -9, e: {sX: 2, sY: 2}}],
-                    [{b: -1, d: 1, o: -1, r: -180, sX: 9, sY: 9}, {b: 2000, d: 1000, o: 1, r: 180, sX: -9, sY: -9, e: {r: 2, sX: 2, sY: 2}}],
-                    [{b: -1, d: 1, o: -1}, {b: 3000, d: 2000, y: 180, o: 1, e: {y: 16}}],
-                    [{b: -1, d: 1, o: -1, r: -150}, {b: 7500, d: 1600, o: 1, r: 150, e: {r: 3}}],
-                    [{b: 10000, d: 2000, x: -379, e: {x: 7}}],
-                    [{b: 10000, d: 2000, x: -379, e: {x: 7}}],
-                    [{b: -1, d: 1, o: -1, r: 288, sX: 9, sY: 9}, {b: 9100, d: 900, x: -1400, y: -660, o: 1, r: -288, sX: -9, sY: -9, e: {r: 6}}, {b: 10000, d: 1600, x: -200, o: -1, e: {x: 16}}]
-                ];
+                                        var jssor_1_SlideoTransitions = [
+                                            [{b: 5500, d: 3000, o: -1, r: 240, e: {r: 2}}],
+                                            [{b: -1, d: 1, o: -1, c: {x: 51.0, t: -51.0}}, {b: 0, d: 1000, o: 1, c: {x: -51.0, t: 51.0}, e: {o: 7, c: {x: 7, t: 7}}}],
+                                            [{b: -1, d: 1, o: -1, sX: 9, sY: 9}, {b: 1000, d: 1000, o: 1, sX: -9, sY: -9, e: {sX: 2, sY: 2}}],
+                                            [{b: -1, d: 1, o: -1, r: -180, sX: 9, sY: 9}, {b: 2000, d: 1000, o: 1, r: 180, sX: -9, sY: -9, e: {r: 2, sX: 2, sY: 2}}],
+                                            [{b: -1, d: 1, o: -1}, {b: 3000, d: 2000, y: 180, o: 1, e: {y: 16}}],
+                                            [{b: -1, d: 1, o: -1, r: -150}, {b: 7500, d: 1600, o: 1, r: 150, e: {r: 3}}],
+                                            [{b: 10000, d: 2000, x: -379, e: {x: 7}}],
+                                            [{b: 10000, d: 2000, x: -379, e: {x: 7}}],
+                                            [{b: -1, d: 1, o: -1, r: 288, sX: 9, sY: 9}, {b: 9100, d: 900, x: -1400, y: -660, o: 1, r: -288, sX: -9, sY: -9, e: {r: 6}}, {b: 10000, d: 1600, x: -200, o: -1, e: {x: 16}}]
+                                        ];
 
-                var jssor_1_options = {
-                    $AutoPlay: true,
-                    $SlideDuration: 800,
-                    $SlideEasing: $Jease$.$OutQuint,
-                    $CaptionSliderOptions: {
-                        $Class: $JssorCaptionSlideo$,
-                        $Transitions: jssor_1_SlideoTransitions
-                    },
-                    $ArrowNavigatorOptions: {
-                        $Class: $JssorArrowNavigator$
-                    },
-                    $BulletNavigatorOptions: {
-                        $Class: $JssorBulletNavigator$
-                    }
-                };
+                                        var jssor_1_options = {
+                                            $AutoPlay: true,
+                                            $SlideDuration: 800,
+                                            $SlideEasing: $Jease$.$OutQuint,
+                                            $CaptionSliderOptions: {
+                                                $Class: $JssorCaptionSlideo$,
+                                                $Transitions: jssor_1_SlideoTransitions
+                                            },
+                                            $ArrowNavigatorOptions: {
+                                                $Class: $JssorArrowNavigator$
+                                            },
+                                            $BulletNavigatorOptions: {
+                                                $Class: $JssorBulletNavigator$
+                                            }
+                                        };
 
-                var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+                                        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
 
-                //responsive code begin
-                //you can remove responsive code if you don't want the slider scales while window resizing
-                function ScaleSlider() {
-                    var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
-                    if (refSize) {
-                        refSize = Math.min(refSize, 1920);
-                        jssor_1_slider.$ScaleWidth(refSize);
-                    } else {
-                        window.setTimeout(ScaleSlider, 30);
-                    }
-                }
-                ScaleSlider();
-                $Jssor$.$AddEvent(window, "load", ScaleSlider);
-                $Jssor$.$AddEvent(window, "resize", ScaleSlider);
-                $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
-                //responsive code end
-            };
+                                        //responsive code begin
+                                        //you can remove responsive code if you don't want the slider scales while window resizing
+                                        function ScaleSlider() {
+                                            var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+                                            if (refSize) {
+                                                refSize = Math.min(refSize, 1920);
+                                                jssor_1_slider.$ScaleWidth(refSize);
+                                            } else {
+                                                window.setTimeout(ScaleSlider, 30);
+                                            }
+                                        }
+                                        ScaleSlider();
+                                        $Jssor$.$AddEvent(window, "load", ScaleSlider);
+                                        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+                                        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+                                        //responsive code end
+                                    };
         </script>
         <script>
             jssor_1_slider_init();
+        </script>
+
+        <script>
+            function myFunction() {
+                document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+            }
         </script>
     </body>
 </html>
