a121c641959b3da110ec7218879bdd8953b39e1b
Rene Teixeira Francisco <renetf77@gmail.com>
2016-06-22 01:41:38 -0300

    Primeiros testes envio de email

--- index.php
+++ index.php
@@ -177,26 +177,72 @@
                                     if (filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING)):
                                         $mensagem = filter_input(INPUT_POST, 'mensagem', FILTER_SANITIZE_STRING);
                                     endif;
-
-//                                if (isset($nome) && isset($email) && isset($telefone) && isset($mensagem)):
-//                                    echo "Dados corretos";
-//                                else:
-//                                    echo "Dados incorretos";
-//                                endif;
-
+                                    if (isset($nome) && isset($email) && isset($mensagem) && isset($telefone)):
+                                        //SMTP needs accurate times, and the PHP time zone MUST be set
+                                        //This should be done in your php.ini, but this is how to do it if you don't have access to that
+                                        date_default_timezone_set('Etc/UTC');
+                                        require './vendor/autoload.php';
+                                        $mail = new PHPMailer;
+                                        $mail->setLanguage('br', '');
+                                        //Tell PHPMailer to use SMTP
+                                        $mail->isSMTP();
+                                        //Enable SMTP debugging
+                                        // 0 = off (for production use)
+                                        // 1 = client messages
+                                        // 2 = client and server messages
+                                        $mail->SMTPDebug = 0;
+                                        //Ask for HTML-friendly debug output
+                                        $mail->Debugoutput = 'html';
+                                        //Set the hostname of the mail server
+                                        $mail->Host = 'smtp.gmail.com';
+                                        // use
+                                        // $mail->Host = gethostbyname('smtp.gmail.com');
+                                        // if your network does not support SMTP over IPv6
+                                        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+                                        $mail->Port = 587;
+                                        //Set the encryption system to use - ssl (deprecated) or tls
+                                        $mail->SMTPSecure = 'tls';
+                                        //Whether to use SMTP authentication
+                                        $mail->SMTPAuth = true;
+                                        //Username to use for SMTP authentication - use full email address for gmail
+                                        $mail->Username = "envio.ibratec@ibratecgrafica.com.br";
+                                        //Password to use for SMTP authentication
+                                        $mail->Password = "";
+                                        //Set who the message is to be sent from
+                                        $mail->setFrom('envio.ibratec@ibratecgrafica.com.br', 'First Last');
+                                        //Set an alternative reply-to address
+//                                        $mail->addReplyTo('replyto@example.com', 'First Last');
+                                        //Set who the message is to be sent to
+                                        $mail->addAddress('renetf77@gmail.com', 'John Doe');
+                                        //Set the subject line
+                                        $mail->Subject = 'PHPMailer GMail SMTP test';
+                                        //Read an HTML message body from an external file, convert referenced images to embedded,
+                                        //convert HTML into a basic plain-text alternative body
+//                                        $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+                                        //Replace the plain text body with one created manually
+                                        $mail->Body = 'This is a plain-text message body';
+                                        //Attach an image file
+//                                        $mail->addAttachment('images/phpmailer_mini.png');
+                                        //send the message, check for errors
+                                        if (!$mail->send()) {
+                                            echo "Mailer Error: " . $mail->ErrorInfo;
+                                        } else {
+                                            echo "Message sent!";
+                                        }
+                                    endif;
                                     ?>
 
                                     <form action="index.php#contato" method="post">
                                         <label>
-                                            <span class="legends">Nome:</span>
+                                            <span class="legends">Nome</span>
                                             <input type="text" name="nome" title="Informe seu nome:" placeholder="Informe seu nome" required/>
                                         </label>
                                         <label class="legends">
-                                            <span>Email:</span>
+                                            <span>Email</span>
                                             <input type="email" name="email" title="Informe seu email:" placeholder="Informe seu email" required/>
                                         </label>
                                         <label>
-                                            <span class="legends">Telefone:</span>
+                                            <span class="legends">Telefone</span>
                                             <input type="tel" name="telefone" title="Informe seu telefone:" placeholder="Informe seu telefone" required/>
                                         </label>
                                         <label>
